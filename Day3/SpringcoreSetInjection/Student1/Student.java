c76038ebd388a5b1cdce03421512eb50d5f84447
samrudhi27 <76120226+samrudhi27@users.noreply.github.com>
2021-08-11 17:06:00 +0530

    Add files via upload

---
+++ Day3/SpringcoreSetInjection/Student1/Student.java
@@ -0,0 +1,41 @@
+package com.springcore;
+
+public class Student {
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	private int studentId;
+	private String studentName;
+	private String studentAddress;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		System.out.println("Setting Student Id");
+		this.studentId = studentId;
+	}
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+				+ "]";
+	}
+	public Student(int studentId, String studentName, String studentAddress) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentAddress = studentAddress;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+}
