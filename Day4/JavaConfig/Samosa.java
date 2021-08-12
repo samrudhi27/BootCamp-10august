a816d789b90f95ae1b0b4d873c28fdb2518ab0af
samrudhi27 <76120226+samrudhi27@users.noreply.github.com>
2021-08-12 13:12:33 +0530

    Add files via upload

---
+++ Day4/JavaConfig/Samosa.java
@@ -0,0 +1,11 @@
+package com.springcore.javaconfig;
+
+
+
+public class Samosa {
+	
+	public void display() {
+		System.out.println("My price is little high....");
+	}
+
+}
---
+++ Day4/JavaConfig/Student.java
@@ -0,0 +1,30 @@
+package com.springcore.javaconfig;
+
+//import org.springframework.stereotype.Component;
+
+//@Component("firststudent")
+public class Student {
+	
+	public Samosa samosa;
+	
+	
+
+	public Samosa getSamosa() {
+		return samosa;
+	}
+
+	public void setSamosa(Samosa samosa) {
+		this.samosa = samosa;
+	}
+
+	public void study()
+	{
+		this.samosa.display();
+		System.out.println("Student is studying");
+	}
+	public Student(Samosa samosa) {
+		super();
+		this.samosa = samosa;
+	}
+
+}
